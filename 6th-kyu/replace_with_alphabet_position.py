63039ab17587d3dbc5489ad1a2b2edf8408349fe
Hector Vega <linnk99@gmail.com>
2021-08-26 23:18:46 -0500

    Replace with alphabet position solved

---
+++ 6th-kyu/replace_with_alphabet_position.py
@@ -0,0 +1,24 @@
+"""
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabet_position("The sunset sets at twelve o' clock.")
+"""
+
+import string
+
+def alphabet_position(text):
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    new_text = ""
+
+    for char in text:
+        if char.lower() in alphabet:
+            new_text += f"{alphabet.index(char.lower()) + 1} "
+
+    return new_text[:-1]
